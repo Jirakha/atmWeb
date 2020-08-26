0d189a67626c4671d7653bb8b749fc7eba0f3df5
จิเรขา ไทยสังคม <jirakha.th@rd.go.th>
2020-08-26 14:37:38 +0700

    customerService

--- src/main/java/th/go/rd/atm/controller/CustomerController.java
+++ src/main/java/th/go/rd/atm/controller/CustomerController.java
@@ -2,27 +2,37 @@ package th.go.rd.atm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 
 @Controller
 public class CustomerController {
 
-    @RequestMapping("/customer")
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+
+        this.customerService = customerService;
+    }
+
+    @GetMapping ("/customer")
     public String getCustomerPage(Model model)
     {
-
-        ArrayList<String> customers = new ArrayList<>();
-        customers.add("Peter");
-        customers.add("Nancy");
-        customers.add("Rick");
-
-        model.addAttribute("allCustomers",customers);
+        model.addAttribute("allCustomers",customerService.getCustomers());
         return "customer"; // customer.html
 
     }
-
+@PostMapping("customer")
+public String registerCustomer(@ModelAttribute Customer customer, Model model)
+{
+     customerService.createCustomer(customer);
+     model.addAttribute("allCustomers", customerService.getCustomers());
+     return "redirect:customer";
+}
 
 
 }
---
+++ src/main/java/th/go/rd/atm/controller/CustomerService.java
@@ -0,0 +1,28 @@
+package th.go.rd.atm.controller;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerService {
+    private ArrayList<Customer> customerList;
+
+@PostConstruct
+public void  postContruct()
+    {
+    customerList = new ArrayList<>();
+
+    }
+    public void createCustomer(Customer customer) {
+        customerList.add(customer);
+    }
+
+    public List<Customer> getCustomers() {
+        return new ArrayList<>(this.customerList);
+    }
+
+
+}
